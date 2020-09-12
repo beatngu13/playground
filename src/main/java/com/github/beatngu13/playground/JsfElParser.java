fb2d34292ec982d1bfa009960c6a34747407e307
Daniel Kraus <daniel.kraus@mailbox.org>
2020-09-12 15:08:16 +0200

    Refactor

--- src/main/java/com/github/beatngu13/playground/JsfElParser.java
+++ src/main/java/com/github/beatngu13/playground/JsfElParser.java
@@ -5,6 +5,7 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,20 +27,25 @@ public class JsfElParser {
 
 	// borrowed from https://stackoverflow.com/a/3940217/3429133
 	private static List<String> getExpressions(String line) {
-		List<String> el = new ArrayList<String>();
 		int i = line.indexOf(EL_START);
 		if (i == -1) {
-			return el;
+			return Collections.emptyList();
 		}
+
+		List<String> expressions = new ArrayList<String>();
 		while (i != -1) {
 			int j = line.indexOf(EL_END, i);
 			if (j == -1) {
-				return el;
+				return Collections.emptyList();
 			}
-			el.add(line.substring(i, j + 1));
-			i = line.indexOf(EL_START, i + 1);
+
+			String expression = line.substring(i, j + 1);
+			expressions.add(expression);
+
+			i = line.indexOf(EL_START, j + 1);
 		}
-		return el;
+
+		return expressions;
 	}
 
 }
