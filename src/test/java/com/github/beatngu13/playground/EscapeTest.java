aef0ad34d247e8c4e7489f17d7ae5f3dbe302d4d
Daniel Kraus <daniel.kraus@mailbox.org>
2021-10-14 18:39:45 +0200

    Add EscapeTest

---
+++ src/test/java/com/github/beatngu13/playground/EscapeTest.java
@@ -0,0 +1,28 @@
+package com.github.beatngu13.playground;
+
+import org.apache.commons.text.StringEscapeUtils;
+import org.junit.jupiter.api.Test;
+
+class EscapeTest {
+
+	@Test
+	void test() {
+		var originalText = """
+				ASCII Text 123.
+				Deutscher Text äöüß.
+				Smiley Text 😉.
+				"""
+				.stripTrailing();
+
+		var escapedText = StringEscapeUtils.escapeJava(originalText);
+
+		var unescapedText = StringEscapeUtils.unescapeJava(originalText);
+
+		System.out.println(originalText);
+		System.out.println("----------");
+		System.out.println(escapedText);
+		System.out.println("----------");
+		System.out.println(unescapedText);
+	}
+
+}
