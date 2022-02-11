97a1479e806540015f18d95f36a4783bc60908f4
Daniel Kraus <daniel.kraus@mailbox.org>
2022-02-12 00:44:53 +0100

    Refactor test

--- src/test/java/com/github/beatngu13/playground/puzzles/BinaryTreePuzzlesTest.java
+++ src/test/java/com/github/beatngu13/playground/puzzles/BinaryTreePuzzlesTest.java
@@ -7,25 +7,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BinaryTreePuzzlesTest {
 
+	Node n5 = new Node(7, null, null);
+	Node n4 = new Node(15, null, null);
+	Node n3 = new Node(8, null, null);
+	Node n2 = new Node(20, n4, n5);
+	Node n1 = new Node(9, n3, null);
+	Node n0 = new Node(3, n1, n2);
+
 	@Test
 	void testGetLevelAverages() {
-		Node n5 = new Node(7, null, null);
-		Node n4 = new Node(15, null, null);
-		Node n3 = new Node(8, null, null);
-		Node n2 = new Node(20, n4, n5);
-		Node n1 = new Node(9, n3, null);
-		Node n0 = new Node(3, n1, n2);
 		assertThat(BinaryTreePuzzles.getLevelAverages(n0)).containsExactly(3.0, 14.5, 10.0);
 	}
 
 	@Test
 	void testGetNextSuccessor() {
-		Node n5 = new Node(7, null, null);
-		Node n4 = new Node(15, null, null);
-		Node n3 = new Node(8, null, null);
-		Node n2 = new Node(20, n4, n5);
-		Node n1 = new Node(9, n3, null);
-		Node n0 = new Node(3, n1, n2);
 		assertThat(BinaryTreePuzzles.getNextSuccessor(n0, 3)).isEqualTo(n1);
 		assertThat(BinaryTreePuzzles.getNextSuccessor(n0, 9)).isEqualTo(n2);
 		assertThat(BinaryTreePuzzles.getNextSuccessor(n0, 15)).isEqualTo(n5);
