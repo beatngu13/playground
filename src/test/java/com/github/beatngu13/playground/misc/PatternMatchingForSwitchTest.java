12b4599ac414f0f1be8e9403aaa7379cb1f7ec42
Daniel Kraus <daniel.kraus@mailbox.org>
2022-05-21 22:50:48 +0200

    Add pattern matching for switch test

---
+++ src/test/java/com/github/beatngu13/playground/misc/PatternMatchingForSwitchTest.java
@@ -0,0 +1,26 @@
+package com.github.beatngu13.playground.misc;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// See: https://openjdk.org/jeps/420
+class PatternMatchingForSwitchTest {
+
+	@ParameterizedTest
+	@CsvSource(textBlock = """
+			4, 0, 0, foo
+			1, 0, 2, bar
+			1, 2, 3, baz
+			""")
+	void test(Integer x, Integer y, Integer z, String expected) {
+		String actual = switch (x) {
+			case 4 -> "foo";
+			case Integer i && y < i && i < z -> "bar";
+			default -> "baz";
+		};
+		assertThat(actual).isEqualTo(expected);
+	}
+
+}
