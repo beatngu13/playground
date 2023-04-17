5b6091fb988607c5e46cdcf98abe406723bdfbc8
Daniel Kraus <daniel.kraus@mailbox.org>
2023-04-17 10:28:37 +0200

    Update pattern matching for switch test

--- src/test/java/com/github/beatngu13/playground/misc/PatternMatchingForSwitchTest.java
+++ src/test/java/com/github/beatngu13/playground/misc/PatternMatchingForSwitchTest.java
@@ -1,26 +1,47 @@
 package com.github.beatngu13.playground.misc;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-// See: https://openjdk.org/jeps/427
+/*
+ * See:
+ * - https://openjdk.org/jeps/441
+ * - https://twitter.com/TheDonRaab/status/1647805135869943814
+ */
 class PatternMatchingForSwitchTest {
 
-	@ParameterizedTest
-	@CsvSource(textBlock = """
-			4, 0, 0, foo
-			1, 0, 2, bar
-			1, 2, 3, baz
-			""")
-	void test(Integer x, Integer y, Integer z, String expected) {
-		String actual = switch (x) {
-			case 4 -> "foo";
-			case Integer i when y < i && i < z -> "bar";
-			default -> "baz";
-		};
-		assertThat(actual).isEqualTo(expected);
+	@Test
+	void test_fizz_buzz() {
+		var list = IntStream.rangeClosed(1, 15)
+				.mapToObj(value ->
+						switch ((Integer) value) {
+							case Integer i when i % 15 == 0 -> "FizzBuzz";
+							case Integer i when i % 5 == 0 -> "Buzz";
+							case Integer i when i % 3 == 0 -> "Fizz";
+							default -> Integer.toString(value);
+						}
+				)
+				.toList();
+		assertThat(list).containsExactly(
+				"1",
+				"2",
+				"Fizz",
+				"4",
+				"Buzz",
+				"Fizz",
+				"7",
+				"8",
+				"Fizz",
+				"Buzz",
+				"11",
+				"Fizz",
+				"13",
+				"14",
+				"FizzBuzz"
+		);
 	}
 
 }
