60018318217396182cb395eb18e6ac968e245341
Daniel Kraus <daniel.kraus@mailbox.org>
2023-11-18 00:30:44 +0100

    Upgrade to Hibernate 6 / Jakarta

--- src/main/java/com/github/beatngu13/playground/hibernate/Book.java
+++ src/main/java/com/github/beatngu13/playground/hibernate/Book.java
@@ -1,12 +1,11 @@
 package com.github.beatngu13.playground.hibernate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.hibernate.envers.Audited;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class Book {
