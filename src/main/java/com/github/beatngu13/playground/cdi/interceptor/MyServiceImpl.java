d2b8638049035a5b5900007b6b37599f8301c631
Daniel Kraus <daniel.kraus@mailbox.org>
2024-02-25 15:19:19 +0100

    Move annotation to type
    Since we want to intercept all (public) methods.

--- src/main/java/com/github/beatngu13/playground/cdi/interceptor/MyServiceImpl.java
+++ src/main/java/com/github/beatngu13/playground/cdi/interceptor/MyServiceImpl.java
@@ -3,16 +3,15 @@ package com.github.beatngu13.playground.cdi.interceptor;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.interceptor.Interceptors;
 
+@Interceptors(MyServiceInterceptor.class)
 @ApplicationScoped
 public class MyServiceImpl implements MyService {
 
-	@Interceptors(MyServiceInterceptor.class)
 	@Override
 	public void foo(String s) {
 		System.out.println(STR. "MyServiceImpl.foo(\{ s })" );
 	}
 
-	@Interceptors(MyServiceInterceptor.class)
 	@Override
 	public void bar(Integer i) {
 		System.out.println(STR. "MyServiceImpl.bar(\{ i })" );
