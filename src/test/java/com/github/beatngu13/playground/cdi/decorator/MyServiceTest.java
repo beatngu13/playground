d726777908529e7490005da818c6c8f38dd13a1a
Daniel Kraus <daniel.kraus@mailbox.org>
2024-02-25 14:22:37 +0100

    Enable test

--- src/test/java/com/github/beatngu13/playground/cdi/decorator/MyServiceTest.java
+++ src/test/java/com/github/beatngu13/playground/cdi/decorator/MyServiceTest.java
@@ -4,10 +4,8 @@ import jakarta.inject.Inject;
 import org.jboss.weld.junit5.auto.AddEnabledDecorators;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 @EnableAutoWeld
 @AddPackages(MyService.class)
 @AddEnabledDecorators(MyServiceDecorator.class)
